481b6d47295999aa2cf8d245458df59bd53022c1
Cole Kuivenhoven <cole.kuivenhoven@quivers.com>
2024-06-12 11:56:15 +0100

    Test

--- services/insights/insights.controller.js
+++ services/insights/insights.controller.js
@@ -13,6 +13,7 @@ dotenv.config()
 exports.getInsight = async (req, res) => {
     console.log('/insights/getInsight');
     const api_key = process.env.OPEN_AI_KEY;
+    console.log('process.env', process.env);
     console.log(' > api key', api_key);
     const file_name = req.body.data_category+"-"+Date.now()+".json";
 
